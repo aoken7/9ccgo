d4df2927953ee1d04c135c59da6232e52fd5db64
build <build@example.com>
2023-11-27 05:49:27 +0000

    Update WASM files

---
+++ wasm_main.go
@@ -0,0 +1,30 @@
+//go:build !test
+
+package main
+
+import (
+	"9ccgo/generator"
+	"9ccgo/lexer"
+	"9ccgo/parser"
+	"syscall/js"
+)
+
+func compile(this js.Value, p []js.Value) interface{} {
+	if len(p) != 1 || p[0].Type() != js.TypeString {
+		return "invalid input"
+	}
+
+	input := p[0].String()
+
+	tokens := lexer.Tokenize(input)
+	parser := parser.New(tokens)
+	ast := parser.Parse()
+
+	result := generator.Compile(ast)
+	return js.ValueOf(result)
+}
+
+func main() {
+	js.Global().Set("compile", js.FuncOf(compile))
+	select {}
+}
